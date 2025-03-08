209a6d1bdef5e7b3fae2391e9e01a759fd82f6de
kaiii299 <67326492+kaiii299@users.noreply.github.com>
2025-03-08 16:37:19 +0800

    Added tailwindcss

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import dash
 from dash import html, dcc, page_container
 
-app = dash.Dash(__name__, use_pages=True)
+external_stylesheets = ["https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"]
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, use_pages=True)
 
 app.layout = html.Div(className="container", children=[
-    html.H1("Flight Map Routing System"),
+    html.H1("Flight Map Routing System", className="text-4xl font-bold text-red-500"),
     
     html.Div(className="section", children=[
         dcc.Link('🌍 Map View', href='/map-view', style={'margin-right': '20px'}),
