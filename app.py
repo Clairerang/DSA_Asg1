9dbf72e4b25d5ba57a8fd4c503e43cd438ea0752
kaiii299 <67326492+kaiii299@users.noreply.github.com>
2025-03-11 21:33:42 +0800

    Added new ui Layout

--- app.py
+++ app.py
@@ -11,14 +11,14 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets, use_pages=T
 app.layout = html.Div(className="flex min-h-screen", children=[
     
     # Sidebar Panel
-    html.Div(className="w-64 bg-white/90 border-r border-gray-200 p-3", children=[
-        html.H2("Flight Routing System", className="text-xl font-bold text-start text-black"),
-        html.Hr(),
+    html.Div(className="w-64 text-white px-0 p-3", children=[
+        html.H2("Flight Routing System", className="text-xl mx-3 font-bold text-start text-white "),
+        html.Hr(className="mx-0"),
         html.Div(id="sidebar-links", className="flex flex-col gap-2"),
     ]),
 
     # Main Content Area
-    html.Div(className="flex-1 p-5", children=[ 
+    html.Div(className="flex-1 px-5", children=[ 
         # Page Container for Routing
         page_container
     ])
@@ -34,15 +34,15 @@ def update_active_link(pathname):
     return [
         dcc.Link(
             "Route View", href="/route-view",
-            className=f"block py-3 px-2 rounded-md {'bg-gray-600 text-white' if pathname == '/route-view' else 'hover:bg-gray-200 transition-colors duration-300'}"
+            className=f"block py-3 px-2 mx-2 rounded-md {'accent-blue text-white' if pathname == '/route-view' else 'hover:bg-gray-100 hover:text-black transition-colors duration-300'}"
         ),
         dcc.Link(
             "Map View", href="/map-view",
-            className=f"block py-3 px-2 rounded-md {'bg-gray-600 text-white' if pathname == '/map-view' else 'hover:bg-gray-200 transition-colors duration-300'}"
+            className=f"block py-3 px-2 mx-2 rounded-md {'accent-blue text-white' if pathname == '/map-view' else 'hover:bg-gray-100 hover:text-black transition-colors duration-300'}"
         ),
         dcc.Link(
             "Table View", href="/table-view",
-            className=f"block py-3 px-2 rounded-md {'bg-gray-600 text-white' if pathname == '/table-view' else 'hover:bg-gray-200 transition-colors duration-300'}"
+            className=f"block py-3 px-2 mx-2 rounded-md {'accent-blue text-white' if pathname == '/table-view' else 'hover:bg-gray-100 hover:text-black transition-colors duration-300'}"
         ),
     ]
 
